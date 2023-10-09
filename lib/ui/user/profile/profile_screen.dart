5416eb924a64b0c982710f20898922c8f930627e
DELL <sjjbsayfiddinov@gmail.com>
2023-10-09 20:00:46 +0500

    some changes

--- lib/ui/user/profile/profile_screen.dart
+++ lib/ui/user/profile/profile_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:market/blocs/user_bloc/user_bloc.dart';
 import 'package:market/cubits/auth_cubit/auth_cubit.dart';
 import 'package:market/data/local/storage_repository.dart';
+import 'package:market/data/models/user/user_model.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -18,19 +20,42 @@ class ProfileScreen extends StatelessWidget {
               context.read<AuthCubit>().logOutUser(context);
               await StorageRepository.deleteString('user_id');
             },
-            icon: const Icon(Icons.logout),
-          ), IconButton(
-            onPressed: () async {
-              context.read<UserBloc>().add(GetUserEvent());
-            },
-            icon: const Icon(Icons.get_app),
+            icon: const Icon(Icons.logout, color: Colors.red),
           ),
         ],
       ),
       body: BlocConsumer<UserBloc, UsersState>(
         builder: (context, state) {
-          return Center(
-            child: Text('Full name ${state.userModel.fullName}'),
+          UserModel user = state.userModel;
+          return Padding(
+            padding: EdgeInsets.all(24.w),
+            child: Column(
+              children: [
+                Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100.r),
+                    child: Image.network(
+                      user.image,
+                      errorBuilder: (context, o, s) => Icon(
+                        Icons.person,
+                        size: 100.w,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10.h),
+                Text(
+                  user.fullName,
+                  style:
+                      TextStyle(fontSize: 24.sp, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 5.h),
+                Text(
+                  '+998 ${user.phone}',
+                  style: TextStyle(fontSize: 18.sp),
+                ),
+              ],
+            ),
           );
         },
         listener: (context, state) {},
