6e0bd5eba7838eba99ab22c8330af8970fce2cad
DELL <sjjbsayfiddinov@gmail.com>
2023-10-10 19:05:10 +0500

    admin profile screen

--- lib/ui/admin/profile/profile_screen.dart
+++ lib/ui/admin/profile/profile_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:market/blocs/user_bloc/user_bloc.dart';
+import 'package:market/cubits/auth_cubit/auth_cubit.dart';
+import 'package:market/data/local/storage_repository.dart';
+import 'package:market/data/models/user/user_model.dart';
 
 class ProfileScreenAdmin extends StatelessWidget {
   const ProfileScreenAdmin({super.key});
@@ -6,7 +12,54 @@ class ProfileScreenAdmin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Profile')),
+      appBar: AppBar(
+        title: const Text('Profile Admin'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              context.read<AuthCubit>().logOutUser(context);
+              await StorageRepository.deleteString('user_id');
+            },
+            icon: const Icon(Icons.logout, color: Colors.red),
+          ),
+        ],
+      ),
+      body: BlocConsumer<UserBloc, UsersState>(
+        builder: (context, state) {
+          UserModel user = state.userModel;
+          return Padding(
+            padding: EdgeInsets.all(24.w),
+            child: Column(
+              children: [
+                Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100.r),
+                    child: Image.network(
+                      user.image,
+                      errorBuilder: (context, o, s) => Icon(
+                        Icons.person,
+                        size: 100.w,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10.h),
+                Text(
+                  user.fullName,
+                  style:
+                  TextStyle(fontSize: 24.sp, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 5.h),
+                Text(
+                  '+998 ${user.phone}',
+                  style: TextStyle(fontSize: 18.sp),
+                ),
+              ],
+            ),
+          );
+        },
+        listener: (context, state) {},
+      ),
     );
   }
 }
--- lib/ui/admin/tabs_box_admin.dart
+++ lib/ui/admin/tabs_box_admin.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:market/blocs/user_bloc/user_bloc.dart';
 import 'package:market/cubits/tabs_box_cubit.dart';
 import 'package:market/ui/admin/add/add_screen.dart';
 import 'package:market/ui/admin/category/category_screen.dart';
@@ -16,8 +17,13 @@ class TabsBoxAdmin extends StatefulWidget {
 class _TabsBoxAdminState extends State<TabsBoxAdmin> {
   List<Widget> screens = [];
 
+  init() {
+    context.read<UserBloc>().add(GetUserEvent());
+  }
+
   @override
   void initState() {
+    init();
     screens.add(const HomeScreenAdmin());
     screens.add(const CategoryScreenAdmin());
     screens.add(const AddScreenAdmin());
@@ -36,6 +42,7 @@ class _TabsBoxAdminState extends State<TabsBoxAdmin> {
         selectedItemColor: Colors.blue,
         showSelectedLabels: true,
         showUnselectedLabels: true,
+        unselectedItemColor: Colors.grey,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(
